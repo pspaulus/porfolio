89a1259a02a46a4b6b3dd47451e92c483f8755b7
Paul Sabando <paul.sabando@funiber.org>
2017-10-31 15:12:33 -0500

    feature: Se agrega interfaz de login

---
+++ application/views/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Login</title>
+
+    <link rel="stylesheet" type="text/css" href="<?=  BASE_URL ?>public/css/bootstrap-reboot.css">
+    <link rel="stylesheet" type="text/css" href="<?=  BASE_URL ?>public/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?=  BASE_URL ?>public/css/font-awesome.css">
+<!--    <link rel="stylesheet" type="text/css" href="--><?//=  BASE_URL ?><!--public/css/estilos.css">-->
+
+    <script src="<?= BASE_URL ?>public/js/jquery.js"></script>
+    <script src="<?= BASE_URL ?>public/js/popper.js"></script>
+    <script src="<?= BASE_URL ?>public/js/bootstrap.js"></script>
+</head>
+
+<body class="--theme-background">
---
+++ application/views/login/index.php
@@ -0,0 +1,36 @@
+<form class="login__form" action="">
+    <div class="container">
+        <br>
+
+        <div class="row justify-content-center">
+            <div class="col-8 col-sm-8 col-md-6 col-lg-4">
+                <h3>Login</h3>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="row justify-content-center">
+            <div class="col-8 col-sm-8 col-md-6 col-lg-4">
+                <div class="form-group input-group">
+                    <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+                    <input type="text" class="form-control" placeholder="Usuario">
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-8 col-sm-8 col-md-6 col-lg-4">
+                <div class="form-group input-group">
+                    <span class="input-group-addon"><i class="fa fa-lock fa-fw"></i></span>
+                    <input type="password" class="form-control" placeholder="Contrase&ntilde;a">
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-4 col-sm-3 col-md-2 col-lg-2">
+                <button class="btn btn-primary btn-block login_button --theme-button">Ingresar</button>
+            </div>
+        </div>
+    </div>
+</form>
